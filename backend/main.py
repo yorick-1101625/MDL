edf6be51e539922ef9fae0b26f485c6223ae6393
yorick <yorick.mol@outlook.com>
2026-05-04 16:26:19 +0200

    Enable CORS

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pytubefix import Playlist
 from pydantic import BaseModel
-
+from starlette.middleware.cors import CORSMiddleware
 
 from backend.services.downloader import download_video_audio, download_playlist_audio
 
@@ -11,8 +11,22 @@ class Url(BaseModel):
 
 
 app = FastAPI()
-
 # /docs or /redoc for api documentation and testing
+
+
+origins = [
+    "http://localhost:5173",
+    # "https://your-production-domain.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post('/download/video')
 async def download(url: Url):
