e0b51b8f81a98537b9604be8c4c4681ac1495d9f
yorick <yorick.mol@outlook.com>
2026-05-05 19:42:31 +0200

    Refactor SkeletonVideo to prevent error div inside p tag

--- frontend/src/components/SkeletonVideo.tsx
+++ frontend/src/components/SkeletonVideo.tsx
@@ -1,4 +1,4 @@
-import {Item, ItemContent, ItemDescription, ItemMedia, ItemTitle} from "@/components/ui/item.tsx";
+import {Item, ItemContent, ItemMedia} from "@/components/ui/item.tsx";
 import {Skeleton} from "@/components/ui/skeleton.tsx";
 
 export default function SkeletonVideo() {
@@ -8,17 +8,11 @@ export default function SkeletonVideo() {
                 <Skeleton className="w-10 h-10 rounded-lg"/>
             </ItemMedia>
             <ItemContent>
-                <ItemTitle className="line-clamp-1 w-full mt-[3.25px]">
-                    <Skeleton className="h-4 max-w-96"/>
-                </ItemTitle>
-                <ItemDescription>
-                    <Skeleton className="h-4 w-20"/>
-                </ItemDescription>
+                <Skeleton className="h-4 max-w-96 mt-[3.25px]"/>
+                <Skeleton className="h-4 w-20 "/>
             </ItemContent>
             <ItemContent className="flex-none text-center">
-                <ItemDescription>
-                    <Skeleton className="w-8 h-4" />
-                </ItemDescription>
+                <Skeleton className="w-8 h-4" />
             </ItemContent>
             <Skeleton className="w-full h-3"/>
         </Item>
--- frontend/src/components/SongDownloader.tsx
+++ frontend/src/components/SongDownloader.tsx
@@ -80,6 +80,7 @@ export default function SongDownloader() {
                         <SkeletonVideo key={value}/>
                     ))
                 }
+                <SkeletonVideo/>
             </ItemGroup>
         </>
     );
